9ee031ab202ad1e271b25ee7d79a21696e2075ac
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-02-16 21:11:57 +0530

    Add files via upload

---
+++ -ve value.c
@@ -0,0 +1,12 @@
+// write a c program to check the given numbers is -ve value
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("Enter the x value:");
+    scanf("%d",&x);
+    int res=(x<0);
+    printf("The %d is negative value is %d",x,res);
+     return 0;
+}
+    
